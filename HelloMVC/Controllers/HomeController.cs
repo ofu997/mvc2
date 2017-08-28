0ef60c94e604f5b5b9b1cd51f8a4723cb3f3a68d
oliver fu <ofu997@gmail.com>
2017-08-28 02:17:15 -0700

    add

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {	
+			// ctrl. makes "using HelloMVC.Models;"
+			var cars = HelloModel.GetCars();
+
+			ViewBag.OliverCreatedThisValue = "~hello MVC~";
+			// generate a view that will be 
+			// used by this actionresult method
+			// this allows us to make a blank page 
+            return View(cars );
+        }
+    }
+}
